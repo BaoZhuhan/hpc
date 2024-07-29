ec2afb69ea6e90d9b30882bc06febe5e4774c14d
bao zhuhan <baozhuhan2004@163.com>
2024-07-30 00:12:15 +0800

    Update task2.cpp

--- wingord/task2.cpp
+++ wingord/task2.cpp
@@ -2,12 +2,19 @@
 using namespace std;
 
 int main() {
-    string a1[2] = { "OpenMP-task-nest" , "OpenMP-task-normal" };
-    string a2[3] = { "sgemm-normal","sgemm-neon-simd","sgemm-V(4*4)" };
-    string a3 = 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << '[' << a1[i] << ']' << "-with-" << '[' << a2[j] << ']' << endl;
+    string a1[3] = { "-openmp-task" , "-openmp-untask" , "-openmp-task-minside" };
+    string a2[2] = { "-U", "-V" };
+    string a3[2] = { "-sgemm","-sgemm-neon" };
+    string a4[2] = { "-sgemm-neon","-sgemm-44" };
+    string a5[2] = { "" , "-check" };
+
+    int cot = 7;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) {
+                cout << cot++ << a1[i] <<  a2[0] << a3[j] << a2[1] << a4[k] << ".sh" << endl;
+                cot++;
+            }
         }
     }
 
